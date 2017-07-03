0f39298587d7550fa5bc56dd021459d42cbbc99e
loganayakin <logusweety13@gmail.com>
2017-07-03 10:01:01 +0530

    Create nl.java

---
+++ nl.java
@@ -0,0 +1,20 @@
+public static void main(String args[])
+{
+int i,m,flag;
+int n=10;
+m=n/2;
+for(i=2;i<=m;i++){
+if(n % i==0)
+{
+System.out.println( " the number is prime);
+
+flag=1;
+}
+}
+break;
+if(flag==0)
+
+System.out.println("the number is not prime");
+}
+}
+
